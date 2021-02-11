090ccb7d33cf028f2de505f5ab903a2c61e5c8c0
drewroberts <github@drewroberts.com>
2021-02-11 12:28:09 -0500

    Consolidate modifying migrations into main migration

--- database/migrations/2020_05_02_150000_create_recurring_schedules_table.php
+++ database/migrations/2020_05_02_150000_create_recurring_schedules_table.php
@@ -11,14 +11,13 @@ class CreateRecurringSchedulesTable extends Migration
         Schema::create('recurring_schedules', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(app('room'))->index();
-            $table->foreignIdFor(app('rate'))->index();
-            $table->string('frequency')->nullable(); // ex.: mon,tue,fri or mon-sun
-            $table->string('time_table')->nullable(); // Location's timezone, not UTC. 34 hour military time. ex.: 18:00:00
+            $table->foreignIdFor(app('rate'))->nullable();
+            $table->smallInteger('day'); // ex.: mon = 1, tue = 2, sun = 7
+            $table->string('time')->nullable(); // Location's timezone, not UTC. 34 hour military time. ex.: 18:00:00
             $table->date('valid_from'); // Will default to today's date if not set.
             $table->date('expires_at')->nullable();
             $table->foreignIdFor(app('user'), 'creator_id');
             $table->foreignIdFor(app('user'), 'updater_id');
-            $table->softDeletes();
             $table->timestamps();
         });
     }
