54adc177c132c9e18aff75c18af8092ccc5ed68c
auxiliawebsitedesign <auxiliawebsitedesign@users.noreply.github.com>
2021-02-13 11:27:18 +0000

    Fix styling

--- src/SchedulingServiceProvider.php
+++ src/SchedulingServiceProvider.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Scheduling;
 
-use Tipoff\Scheduling\Models\Block;
-use Tipoff\Scheduling\Models\Game;
-use Tipoff\Scheduling\Models\Hold;
-use Tipoff\Scheduling\Models\RecurringSchedule;
-use Tipoff\Scheduling\Models\ScheduleEraser;
-use Tipoff\Scheduling\Models\Slot;
 use Tipoff\Scheduling\Policies\SchedulingPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
