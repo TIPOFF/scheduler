cf1a826327e580ca3f5a10d2973e2d85582f07f3
auxiliawebsitedesign <preston@auxiliawebsitedesign.com>
2021-02-13 06:26:43 -0500

    Update model list

--- src/SchedulingServiceProvider.php
+++ src/SchedulingServiceProvider.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Tipoff\Scheduling;
 
-use Tipoff\Scheduling\Models\Scheduling;
+use Tipoff\Scheduling\Models\Block;
+use Tipoff\Scheduling\Models\Game;
+use Tipoff\Scheduling\Models\Hold;
+use Tipoff\Scheduling\Models\RecurringSchedule;
+use Tipoff\Scheduling\Models\ScheduleEraser;
+use Tipoff\Scheduling\Models\Slot;
 use Tipoff\Scheduling\Policies\SchedulingPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
