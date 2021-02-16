092d031b4391daba4c5498a5aa7809532a7d20d4
kylebarney <kylebarney@users.noreply.github.com>
2021-02-16 05:26:07 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Tipoff\Scheduling\Tests;
 
-use Tipoff\Scheduling\SchedulingServiceProvider;
-use Tipoff\Support\SupportServiceProvider;
 use Tipoff\EscapeRoom\EscapeRoomServiceProvider;
 use Tipoff\Locations\LocationsServiceProvider;
+use Tipoff\Scheduling\SchedulingServiceProvider;
+use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 
 class TestCase extends BaseTestCase
