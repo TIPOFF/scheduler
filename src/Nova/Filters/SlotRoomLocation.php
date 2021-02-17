7e77dc229a7ac66a4dbda16eb24f7301b49e0cca
devjk1 <jinsik@omniadigital.io>
2021-02-17 13:35:53 -0500

    [TGER-68] migrate nova filters

---
+++ src/Nova/Filters/SlotRoomLocation.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class SlotRoomLocation extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * The displayable name of the filter.
+     *
+     * @var string
+     */
+    public $name = 'Location';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query
+            ->join('slots', 'slots.id', 'slot_id')
+            ->join('rooms', 'rooms.id', 'slots.room_id')
+            ->where('rooms.location_id', $value)
+            ->get();
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return \App\Models\Location::pluck('id', 'name');
+    }
+}
