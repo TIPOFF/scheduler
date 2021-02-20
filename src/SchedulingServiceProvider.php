a4d2e3e6b21de9dcaf2f539304b673c7b92e0a2d
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-02-20 15:21:45 -0500

    eliminate unnecessary calls and the Slot class from schedulingServiceProvider

--- src/SchedulingServiceProvider.php
+++ src/SchedulingServiceProvider.php
@@ -8,12 +8,10 @@ use Tipoff\Scheduling\Models\Block;
 use Tipoff\Scheduling\Models\Game;
 use Tipoff\Scheduling\Models\RecurringSchedule;
 use Tipoff\Scheduling\Models\ScheduleEraser;
-use Tipoff\Scheduling\Models\Slot;
 use Tipoff\Scheduling\Policies\BlockPolicy;
 use Tipoff\Scheduling\Policies\GamePolicy;
 use Tipoff\Scheduling\Policies\RecurringSchedulePolicy;
 use Tipoff\Scheduling\Policies\ScheduleEraserPolicy;
-use Tipoff\Scheduling\Policies\SlotPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -27,7 +25,6 @@ class SchedulingServiceProvider extends TipoffServiceProvider
                 Game::class => GamePolicy::class,
                 RecurringSchedule::class => RecurringSchedulePolicy::class,
                 ScheduleEraser::class => ScheduleEraserPolicy::class,
-                Slot::class => SlotPolicy::class,
             ])
             ->name('scheduling')
             ->hasConfigFile();
