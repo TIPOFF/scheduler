bf8a6da18f02b9988b16d50271da0677ac8423f2
dj <djamesfar@gmail.com>
2021-03-06 08:08:50 -0800

    use new BasePermissionsMigration; bump packages

--- src/Models/Block.php
+++ src/Models/Block.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tipoff\Scheduler\Models;
 
-use Tipoff\Bookings\Model\Slot;
+use Tipoff\Bookings\Models\Slot;
 use Tipoff\Support\Models\BaseModel;
 use Tipoff\Support\Traits\HasCreator;
 use Tipoff\Support\Traits\HasPackageFactory;
