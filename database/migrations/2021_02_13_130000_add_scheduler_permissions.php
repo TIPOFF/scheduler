4c1d4f284376f0904f50e32b8c91bcb03869bfed
dj <djamesfar@gmail.com>
2021-03-06 07:19:43 -0800

    use new BasePermissionsMigration

--- database/migrations/2021_02_13_130000_add_scheduler_permissions.php
+++ database/migrations/2021_02_13_130000_add_scheduler_permissions.php
@@ -2,31 +2,24 @@
 
 declare(strict_types=1);
 
-use Illuminate\Database\Migrations\Migration;
-use Spatie\Permission\Contracts\Permission;
-use Spatie\Permission\PermissionRegistrar;
+use Tipoff\Authorization\Permissions\BasePermissionsMigration;
 
-class AddSchedulerPermissions extends Migration
+class AddSchedulerPermissions extends BasePermissionsMigration
 {
     public function up()
     {
-        if (app()->has(Permission::class)) {
-            app(PermissionRegistrar::class)->forgetCachedPermissions();
-
-            foreach ([
-                     'view blocks',
-                     'create blocks',
-                     'update blocks',
-                     'delete blocks',
-                     'view games',
-                     'update games',
-                     'view schedules',
-                     'create schedules',
-                     'update schedules',
-                     'delete schedules',
-                 ] as $name) {
-                app(Permission::class)::findOrCreate($name, null);
-            };
-        }
+        $permissions = [
+             'view blocks',
+             'create blocks',
+             'update blocks',
+             'delete blocks',
+             'view games',
+             'update games',
+             'view schedules',
+             'create schedules',
+             'update schedules',
+             'delete schedules',
+        ];
+        $this->createPermissions($permissions);
     }
 }
