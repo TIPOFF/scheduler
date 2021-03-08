a6ff36d7cb9745e2e12645caf2029365c08a9d03
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-08 17:52:02 -0500

    Add dependencies to the testCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Tipoff\Scheduler\Tests;
 
+use Laravel\Nova\NovaCoreServiceProvider;
 use Tipoff\EscapeRoom\EscapeRoomServiceProvider;
 use Tipoff\Locations\LocationsServiceProvider;
 use Tipoff\Scheduler\SchedulerServiceProvider;
+use Tipoff\Scheduler\Tests\Support\Providers\NovaPackageServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 
@@ -19,6 +21,8 @@ class TestCase extends BaseTestCase
             LocationsServiceProvider::class,
             EscapeRoomServiceProvider::class,
             SchedulerServiceProvider::class,
+            NovaCoreServiceProvider::class,
+            NovaPackageServiceProvider::class,
         ];
     }
 }
