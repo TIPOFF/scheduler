98601f0d4562a9a81e204c0383b0f78d16d1b093
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-08 17:51:44 -0500

    add NovaCoreService Provider

---
+++ tests/Unit/Support/Providers/NovaPackageServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Scheduler\Tests\Support\Providers;
+
+use Tipoff\Scheduler\Nova\Block;
+use Tipoff\Scheduler\Nova\Game;
+use Tipoff\Scheduler\Nova\RecurringSchedule;
+use Tipoff\Scheduler\Nova\ScheduleErarser;
+use Tipoff\Scheduler\Nova\Slot;
+use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
+
+class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
+{
+    public static array $packageResources = [
+        Block::class,
+        Game::class,
+        RecurringSchedule::class,
+        ScheduleErarser::class,
+        Slot::class,
+    ];
+}
