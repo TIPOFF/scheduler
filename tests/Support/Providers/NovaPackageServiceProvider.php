84ad94a22a78566010af87217f765296db59586a
drewroberts <drewroberts@users.noreply.github.com>
2021-03-16 20:23:36 +0000

    Fix styling

--- tests/Support/Providers/NovaPackageServiceProvider.php
+++ tests/Support/Providers/NovaPackageServiceProvider.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Tipoff\Scheduler\Tests\Support\Providers;
 
 use Tipoff\Scheduler\Nova\Block;
+use Tipoff\Scheduler\Nova\EscaperoomSlot;
 use Tipoff\Scheduler\Nova\Game;
 use Tipoff\Scheduler\Nova\RecurringSchedule;
 use Tipoff\Scheduler\Nova\ScheduleErarser;
-use Tipoff\Scheduler\Nova\EscaperoomSlot;
 use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
 
 class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
