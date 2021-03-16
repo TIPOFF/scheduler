4f2fb71700100ba50cf0d799031ffcb8762890d5
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-16 18:26:36 -0400

    fix duplicate class

--- tests/Support/Providers/NovaPackageServiceProvider.php
+++ tests/Support/Providers/NovaPackageServiceProvider.php
@@ -9,7 +9,6 @@ use Tipoff\Scheduler\Nova\EscaperoomSlot;
 use Tipoff\Scheduler\Nova\Game;
 use Tipoff\Scheduler\Nova\RecurringSchedule;
 use Tipoff\Scheduler\Nova\ScheduleErarser;
-use Tipoff\Scheduler\Nova\EscaperoomSlot;
 use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
 
 class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
