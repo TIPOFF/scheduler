9ec874ad7bace08933d78ad0b0fb754abc8b3be8
Hunter Montell <64396917+huntermontell@users.noreply.github.com>
2021-03-17 14:15:48 -0500

    Update Data Fields Panel on Nova Resources

--- src/Nova/Block.php
+++ src/Nova/Block.php
@@ -91,13 +91,12 @@ class Block extends BaseResource
         ]);
     }
 
-    protected function dataFields():array
+    protected function dataFields(): array
     {
-        return array_filter([
-            ID::make(),
-            nova('user') ? BelongsTo::make('Creator', 'creator', nova('user'))->exceptOnForms() : null,
-            DateTime::make('Created At')->exceptOnForms(),
-            DateTime::make('Updated At')->exceptOnForms(),
-        ]);
+        return array_merge(
+            parent::dataFields(),
+            $this->creatorDataFields(),
+            $this->updaterDataFields(),
+        );
     }
 }
