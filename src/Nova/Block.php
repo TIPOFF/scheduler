412a03f6afb58acb0ed538909c4969ea243c8994
huntermontell <huntermontell@users.noreply.github.com>
2021-03-17 19:16:09 +0000

    Fix styling

--- src/Nova/Block.php
+++ src/Nova/Block.php
@@ -7,7 +7,6 @@ namespace Tipoff\Scheduler\Nova;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Date;
-use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\MorphMany;
 use Laravel\Nova\Fields\Number;
