f8715a1d37a16cecddaeaa58cb5de6e69eb936d3
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-22 12:11:58 -0400

    add unit test for block model

--- tests/Unit/Models/BlockModelTest.php
+++ tests/Unit/Models/BlockModelTest.php
@@ -6,6 +6,7 @@ namespace Tipoff\Scheduler\Tests\Unit\Models;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tipoff\Scheduler\Models\Block;
+use Tipoff\Scheduler\Models\EscaperoomSlot;
 use Tipoff\Scheduler\Tests\TestCase;
 
 class BlockModelTest extends TestCase
@@ -17,5 +18,24 @@ class BlockModelTest extends TestCase
     {
         $model = Block::factory()->create();
         $this->assertNotNull($model);
+        return $model;
+    }
+
+    /**
+    @test
+    *@depends create
+    */
+    public function it_has_participants(Block $model)
+    {
+        $this->assertIsInt($model->participants);
+    }
+
+    /**
+    @test
+    */
+    public function it_has_a_slot()
+    {
+        $model = Block::factory()->create();
+        $this->assertInstanceOf(EscaperoomSlot::class, $model->slot);
     }
 }
