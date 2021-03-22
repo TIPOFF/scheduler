821691a0211e854295c4f3e8a4112fea75e64b7c
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-22 10:53:07 -0400

    misspelled a letter

--- database/migrations/2020_05_04_100000_create_escaperoom_slots_table.php
+++ database/migrations/2020_05_04_100000_create_escaperoom_slots_table.php
@@ -20,7 +20,7 @@ class CreateEscaperoomSlotsTable extends Migration
             $table->date('date')->index();
             $table->dateTime('start_at');
             $table->dateTime('end_at'); // Computed from room->duration
-            $table->dateTime('room_availale_at'); // Time when the room is available for another slot. Automatically computed by adding room->occupied_time to slot->start_at
+            $table->dateTime('room_available_at'); // Time when the room is available for another slot. Automatically computed by adding room->occupied_time to slot->start_at
             $table->unsignedTinyInteger('participants_booked');
             $table->unsignedTinyInteger('participants_blocked');
             $table->unsignedTinyInteger('participants_available'); // Capacity - defaults to room capacity for public games
