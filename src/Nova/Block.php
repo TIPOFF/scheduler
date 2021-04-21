06b628010fa1f3ff8e8878cc356f24c371eb5c1a
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-04-21 15:38:44 -0400

    fix leftJoin with the proper table relation

--- src/Nova/Block.php
+++ src/Nova/Block.php
@@ -45,7 +45,7 @@ class Block extends BaseResource
         ])) {
             return $query
                 ->select('blocks.*')
-                ->leftJoin('slots as slot', 'slot.id', '=', 'blocks.escaperoom_slot_id')
+                ->leftJoin('escaperoom_slots as slot', 'slot.id', '=', 'blocks.escaperoom_slot_id')
                 ->leftJoin('rooms as room', 'room.id', '=', 'slot.room_id');
         }
 
@@ -53,7 +53,7 @@ class Block extends BaseResource
             return $orderlocation
                 ->whereIn('room.location_id', $request->user()->locations->pluck('id'));
         })->select('blocks.*')
-            ->leftJoin('slots as slot', 'slot.id', '=', 'blocks.escaperoom_slot_id')
+            ->leftJoin('escaperoom_slots as slot', 'slot.id', '=', 'blocks.escaperoom_slot_id')
             ->leftJoin('rooms as room', 'room.id', '=', 'slot.room_id');
     }
 
